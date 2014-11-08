29844d3b20809434360333da899e21273f10fa5d
Ilya Ulyanov <ilya.wt@gmail.com>
2014-11-08 11:41:32 -0800

    initial

---
+++ application/views/index/index.php
@@ -0,0 +1,16 @@
+<div class="content">
+    <h1>Projek - Test Info Below Please Ignore</h1>
+
+    <!-- echo out the system feedback (error and success messages) -->
+    <?php $this->renderFeedbackMessages(); ?>
+
+    <p>
+        This box (everything between header and footer) is the content of views/index/index.php,
+        so it's the index/index view.
+        <br/>
+        It's rendered by the index-method within the index-controller (in controllers/index.php).
+    </p>
+    <p>
+        <h2>Framework #1000</h2>
+    </p>
+</div>
