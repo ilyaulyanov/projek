4efbbb74c29e160acb7f781b934309a2ebab2475
draytonvanas <draytonvanasapps@gmail.com>
2014-11-25 12:35:11 -0800

    Forgot password styling done

--- application/views/login/requestpasswordreset.php
+++ application/views/login/requestpasswordreset.php
@@ -1,15 +1,21 @@
+<div class="row">
+          <div class="small-10 small-centered columns">
 <div class="content">
-    <h1>Request a password reset</h1>
+        <div class="register-box">
+    <h2 class="text-center">Reset Password</h2>
 
     <!-- echo out the system feedback (error and success messages) -->
     <?php $this->renderFeedbackMessages(); ?>
 
     <!-- request password reset form box -->
     <form method="post" action="<?php echo URL; ?>login/requestpasswordreset_action" name="password_reset_form">
-        <label for="password_reset_input_username">
+        <p>
             Enter your email and you'll get a mail with instructions:
-        </label>
+        </p>
         <input id="password_reset_input_username" class="password_reset_input" type="email" name="user_name" required />
-        <input type="submit"  name="request_password_reset" value="Reset my password" />
+        <input type="submit" id="request-password-button" class="button" name="request_password_reset" value="Reset my password" />
     </form>
+        </div>
+</div>
+</div>
 </div>
