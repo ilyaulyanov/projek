b9217328741ea0f827ad44d9bf60b9f2a7e6a4cf
draytonvanas <draytonvanasapps@gmail.com>
2014-11-25 14:50:17 -0800

    Project progress page styled

--- application/views/project/index.php
+++ application/views/project/index.php
@@ -1,17 +1,17 @@
-
+<div class="row">
+          <div class="small-10 small-centered columns">
 
 <script src="<?php echo URL; ?>public/js/vendor/jquery.js"></script>
         <script src="<?php echo URL; ?>public/js/foundation.min.js"></script>
 <div class="content" >
-    <h1 style="margin-top: 50px;">Project Timeline</h1>
+    <h2 class="text-center">Project Timeline</h2>
 
         <?php 
         $project = $this->project;
-            echo "<h2 class='subheader'>{$project['projectName']}</h2>";
-            echo "<h3><small>{$project['projectDesc']}</small></h3>";
+            echo "<h3 class='subheader text-center'>{$project['projectName']}</h3>";
+            echo "<h3 class='text-center'><small>{$project['projectDesc']}</small></h3>";
 
             $stagesArray = $project['stages'];
-            print_r($stagesArray);
             echo "<div class='stages_wrapper'>";
             $i = 0;
             foreach ($stagesArray as $stage_name => $stageArr) {
@@ -21,7 +21,7 @@
                        # code...
                     echo "<h3><small>".$task."</small></h3>";
                     echo "<div class='row'>
-  <div class='small-10 medium-11 columns'>
+  <div class='small-12 medium-11 columns'>
     <div class='range-slider' data-slider>
       <span class='range-slider-handle' role='slider' tabindex='0'></span>
       <span class='range-slider-active-segment'></span>
@@ -38,15 +38,7 @@
         ?>
         <div id="msg"></div>
 
-        <div class='row'>
-          <div class='small-10 medium-11 columns'>
-            <div class='range-slider' data-slider>
-              <span class='range-slider-handle' role='slider' tabindex='0'></span>
-              <span class='range-slider-active-segment'></span>
-            </div>
-          </div>
-
-        </div> 
-
 </div>
 
+</div>
+</div>
