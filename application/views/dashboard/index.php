58d604fea5d116e6dfb0dce5f0f35b982e1ed2e1
draytonvanas <draytonvanasapps@gmail.com>
2014-11-25 14:21:54 -0800

    Dashboard done - links still don't work (project edit and delete)

--- application/views/dashboard/index.php
+++ application/views/dashboard/index.php
@@ -1,20 +1,35 @@
-<div class="content">
-    <h1>Dashboard</h1>
-    <div>
-        Your username: <?php echo Session::get('user_name'); ?>
-    </div>
-    <div>
-        Your email: <?php echo Session::get('user_email'); ?>
-    </div>
-    <div>
-        Your avatar image:
+    <h2 class="text-center">Dashboard</h2>
+<div class="row">
+    <div class="content">
+    <div class="small-4 large-4 columns">
+            <div class="avatarImg">
         <?php // if usage of gravatar is activated show gravatar image, else show local avatar ?>
         <?php if (USE_GRAVATAR) { ?>
-            Your gravatar pic (on gravatar.com): <img src='<?php echo Session::get('user_gravatar_image_url'); ?>' />
+            <img src='<?php echo Session::get('user_gravatar_image_url'); ?>' />
         <?php } else { ?>
-            Your avatar pic (saved on local server): <img src='<?php echo Session::get('user_avatar_file'); ?>' />
+            <img src='<?php echo Session::get('user_avatar_file'); ?>' />
         <?php } ?>
+            <div class="editAvatar text-center">
+            <a href="<?php echo URL; ?>login/uploadavatar">Edit Avatar</a>
+            </div>
     </div>
+    </div>
+    <div class="small-8 large-4 columns">
+            <div class="usernameDashboard text-left">
+            <?php echo Session::get('user_name'); ?>
+            </div>
+            <div class="editProfileText">
+            <a href="<?php echo URL; ?>login/editusername">Edit Username</a>
+            |
+            <a href="<?php echo URL; ?>login/edituseremail">Edit Email</a>
+            </div>
+    </div>
+
+</div>
+</div>
+<div class="row">
+    <div class="editProfileDashboard">
+          <div class="small-10 small-centered columns">
     <!-- echo out the system feedback (error and success messages) -->
     <?php $this->renderFeedbackMessages(); ?>
 	<table>
@@ -23,14 +38,15 @@
             foreach($this->projects as $key => $value) {
                 echo '<tr>';
                 echo '<td>' . htmlentities($value->project_name) . '</td>';
-                echo '<td><a href="'. URL . 'project/edit/' . $value->project_id.'">Edit</a></td>';
-                echo '<td><a href="'. URL . 'project/delete/' . $value->project_id.'">Delete</a></td>';
+                echo '<td><a href="'. URL . '/project' . $value->project_id.'">Edit</a></td>';
+                echo '<td><a href="'. URL . '/project' . $value->project_id.'">Delete</a></td>';
                 echo '</tr>';
             }
         } else {
-            echo 'No projects yet. <a href="'. URL . 'project/create/"">Create some!</a>';
+            echo 'No projects yet. <a href="'. URL . 'project/create/"">Create Project</a>';
         }
     ?>
     </table>
-    <h2> Items to have on the dashboard page: user profile pic, name, links to change name / pic, current project[done] </h2>
+</div>
+</div>
 </div>
