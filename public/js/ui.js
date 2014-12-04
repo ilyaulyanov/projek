cae5070123273464b79abc1b6c9c97d5a0b9199f
Ilya Ulyanov <ilya.wt@gmail.com>
2014-12-04 12:36:41 -0800

    Save task for project/index

--- public/js/ui.js
+++ public/js/ui.js
@@ -9,10 +9,65 @@ $(document).ready(function(){
 	})
 */
 
-	$('[data-slider]').on('change.fndtn.slider', function($this){
-  // do something when the value changes
-  console.log($(this).attr("data-slider"));
-});
+
+	//$('div>range-slider-handle').foundation('reflow');
+	var handle = document.getElementsByClassName('range-slider-handle');
+	for (var i = 0; i < handle.length; i++) {
+		assignManager(handle[i]);
+	};
+
+	function assignManager(handle){
+		var handleEl = handle;
+		var mc = new Hammer.Manager(handleEl);
+		mc.add( new Hammer.Pan({ direction: Hammer.DIRECTION_ALL, threshold:0}));
+		mc.add( new Hammer.Tap({ event: 'tap', taps: 1}));
+		mc.add( new Hammer.Press({ event: 'pressup'}))
+
+		mc.on("panend", function(ev, $this){handlePan(ev, this);});
+		mc.on("tap", handleTaps);
+		mc.on('pressup', function(ev){handlePressUp(ev);});
+	}
+	function handlePan(e, obj){
+		var obj = e.target;
+		var value = $(obj).attr("aria-valuenow");
+		console.log(obj);
+		console.log(value);
+		saveTask(obj,value);
+	}
+
+	function handlePressUp(){
+		console.log('pressup');
+	}
+	
+
+	function handleTaps(){
+		console.log('yaya');
+	}
+
+	function saveTask(obj,value){
+		var saveBtn = document.createElement('div');
+		saveBtn.setAttribute("class","btn_save_task");
+		var saveBtnText = document.createTextNode("Save");
+		saveBtn.appendChild(saveBtnText);
+		var found = $(obj).closest('.content');
+		console.log(found[0]);
+		
+			if($('.btn_save_task').length > 0){
+				return false;
+			}else{
+				$(found).prepend(saveBtn);
+			}
+		
+		//console.log(find_btn[0]);
+		
+		
+
+
+		
+
+	}
+	
+
 
 })
 
